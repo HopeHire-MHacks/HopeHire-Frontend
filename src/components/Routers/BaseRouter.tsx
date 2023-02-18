8c605d0122b42790207944b4631c73f993109260
ShenyiCui <shenyicui@outlook.com>
2023-02-18 10:27:31 +0800

    :tada: init: routes

--- src/components/Routers/BaseRouter.tsx
+++ src/components/Routers/BaseRouter.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import { routes } from '@/constants/routes';
 import Home from '@pages/Home';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 const BaseRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.home} element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path={routes.home} component={Home} />
+      <Route exact path='*'>
+        <Redirect to={routes.home} />
+      </Route>
+    </Switch>
   );
 };
 
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,22 @@
 export const routes = {
   home: '/',
+  authentication: {
+    login: '/login',
+    signup: '/signup',
+  },
+  onboard: '/onboard',
+  landing: {
+    features: '/#features',
+    stories: '/#stories',
+    patients: '/#patients',
+    employers: '/#employers',
+  },
+  employer: {
+    base: '/employer',
+    newListing: '/newListing',
+    listings: '/listings',
+    listing: '/listing',
+    applications: '/applications',
+    profile: '/profile',
+  },
 };
