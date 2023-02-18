ebcad6a617c84817dbc93fb2075ae75101d8aa84
ShenyiCui <shenyicui@outlook.com>
2023-02-18 11:33:19 +0800

    :tada: init: login page

---
+++ src/pages/Landing/Login/Login.tsx
@@ -0,0 +1,20 @@
+import BottomBackground from '@/components/Landing/Background/BottomBackground';
+import LoginForm from '@/components/Landing/Forms/Authentication/LoginForm';
+import NavigationBar from '@/components/Landing/NavigationBar';
+import React from 'react';
+import TopBackground from '@/components/Landing/Background/TopBackground';
+
+const Login = () => {
+  return (
+    <div className='isolate bg-white'>
+      <TopBackground></TopBackground>
+      <NavigationBar></NavigationBar>
+      <main>
+        <LoginForm></LoginForm>
+        <BottomBackground></BottomBackground>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
