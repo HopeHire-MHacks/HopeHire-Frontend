b54a3c62d866e837efb60ca1dcd0605eb1306d30
ShenyiCui <shenyicui@outlook.com>
2023-02-18 10:47:10 +0800

    :sparkles: feat: add API employer service

---
+++ src/api/Employer/EmployerService.ts
@@ -0,0 +1,60 @@
+import ApiService, { ApiData } from '@/api/ApiService';
+import { convertToBlob } from '@/utils/miscellaneous';
+
+export type CreateEmployerData = {
+  companyName: string;
+  companyWebsite: string;
+  companyDescription: string;
+  logo: string;
+  address: string;
+  userId: number;
+  latLong: number[];
+  numberOfEmployees: number;
+};
+
+export default class EmployerService {
+  private static getEmployerUrl() {
+    return 'employers';
+  }
+
+  public static async createEmployer(createEmployerData: CreateEmployerData): Promise<ApiData> {
+    const imageBlob = convertToBlob(createEmployerData.logo);
+
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getEmployerUrl()}`,
+          method: 'POST',
+          data: {
+            name: createEmployerData.companyName,
+            companyDescription: createEmployerData.companyDescription,
+            logo: imageBlob,
+            address: createEmployerData.address,
+            numberOfEmployees: createEmployerData.numberOfEmployees,
+            latLong: createEmployerData.latLong,
+            userId: createEmployerData.userId,
+          },
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getAllEmployers(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: this.getEmployerUrl(),
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
