f048a33da49605cbcd3ada019def787d2fe52b9f
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-02-18 23:39:37 +0800

    :recycle: refactor CardListWithHeader component

--- src/components/Employee/Listings/Listings.tsx
+++ src/components/Employee/Listings/Listings.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CardListWithHeader from '../CardListWithHeader';
 import ListingItem from '../ListingItem';
 
 const positions = [
@@ -114,38 +115,15 @@ const positions = [
 
 const Listings = () => {
   return (
-    <>
-      <div className='m-5 w-full'>
-        <div className='overflow-hidden rounded-lg bg-white shadow'>
-          {' '}
-          <div className='border-b border-gray-200 bg-white px-4 py-5 sm:px-6'>
-            <div className='-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap'>
-              <div className='ml-4 mt-4'>
-                <h3 className='text-lg font-medium leading-6 text-gray-900'>Job Postings</h3>
-                <p className='mt-1 text-md text-gray-500'>Discover your next career opportunity with our extensive job listings.</p>
-              </div>
-              <div className='ml-4 mt-4 flex-shrink-0'>
-                <button
-                  type='button'
-                  className='relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-md font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                >
-                  View Submitted Applications
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className=' divide-gray-200 border-t border-gray-200 bg-gray-50 sm:grid-cols-3 sm:divide-y-0 sm:divide-x'>
-          <div className='overflow-hidden bg-white shadow sm:rounded-md'>
-            <ul role='list' className='divide-y divide-gray-200'>
-              {positions.map(position => (
-                <ListingItem key={position.id} position={position} />
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+    <CardListWithHeader
+      header='Job Postings'
+      description='Discover your next career opportunity with our extensive job listings.'
+      buttonText='View Submitted Applications'
+    >
+      {positions.map(position => (
+        <ListingItem key={position.id} position={position} />
+      ))}
+    </CardListWithHeader>
   );
 };
 
