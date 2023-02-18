f3c2d6abbdee390b90681012a713df0eb0230691
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-02-18 23:00:13 +0800

    :sparkles: create job listing item component

---
+++ src/components/Employee/ListingItem/ListingItem.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { CalendarIcon, MapPinIcon, UsersIcon } from '@heroicons/react/20/solid';
+import { ReactComponent as HealthIcon } from '@/assets/health_icon.svg';
+
+import industryTypes from '@/constants/industryTypes';
+
+interface Job {
+  id: number;
+  positionName: string;
+  jobType: string;
+  industryType: number;
+  jobDescription: string;
+  jobRequirements: string;
+  jobFlexibility: string;
+  latLong: number[];
+  hasDialysisSupport: boolean;
+  hasFlexibleSchedule: boolean;
+  physicalDemands: string;
+  salaryType: string;
+  scheduledType: string;
+  openingTime: Date;
+  employerId: number;
+  isOpen: boolean;
+  salaryRange: number[];
+  skills: number[];
+  country: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  address: string;
+}
+
+interface Props {
+  position: Job;
+}
+
+const ListingItem = ({ position }: Props) => {
+  return (
+    <>
+      <li key={position.id}>
+        <a href='#' className='block hover:bg-gray-50'>
+          <div className='px-4 py-4 sm:px-6 sm:pl-2 flex'>
+            <div className='flex items-center w-12 justify-between'>
+              <img
+                className='h-12 w-12 rounded-full'
+                src={
+                  'https://media.licdn.com/dms/image/C560BAQE88xCsONDULQ/company-logo_100_100/0/1618231291419?e=1684972800&v=beta&t=77OtMEMQV6GEYwg9e5OtRSjKu1OVwoGBrXz_Dk_WSog'
+                }
+                alt=''
+              />
+            </div>
+            <div className='w-full pl-3'>
+              <div className='flex items-center justify-between'>
+                <p className='truncate text-md font-medium text-indigo-600'>{position.positionName}</p>
+                <div className='ml-2 flex flex-shrink-0'>
+                  <p className='inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800'>
+                    {position.scheduledType}
+                  </p>
+                </div>
+              </div>
+              <div className='mt-2 sm:flex sm:justify-between'>
+                <div className='sm:flex'>
+                  <p className='flex items-center text-md text-gray-500'>
+                    <UsersIcon className='mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
+                    {industryTypes[position.industryType]}
+                  </p>
+                  <p className='mt-2 flex items-center text-md text-gray-500 sm:mt-0 sm:ml-6'>
+                    <MapPinIcon className='mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
+                    {position.city}
+                  </p>
+                  {position.hasDialysisSupport && (
+                    <p className={'mt-2 flex items-center text-md text-green-500 sm:mt-0 sm:ml-6'}>
+                      <HealthIcon fill='#22C55E' className='mr-1.5 h-6 w-6 flex-shrink-0 text-gray-400' aria-hidden='true' />
+                      Peritoneal Dialysis-Friendly
+                    </p>
+                  )}
+                </div>
+                <div className='mt-2 flex items-center text-md text-gray-500 sm:mt-0'>
+                  <CalendarIcon className='mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400' aria-hidden='true' />
+                  <p>Closing Tomorrow</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </li>
+    </>
+  );
+};
+
+export default ListingItem;
