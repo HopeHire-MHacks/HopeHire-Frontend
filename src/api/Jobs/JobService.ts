802bc5743469cabe5796039d6ca4378f11f44f53
yunruu <yunruuchin@gmail.com>
2023-02-19 01:32:47 +0800

    :sparkles: `feat: add JobService API`

--- src/api/Jobs/JobService.ts
+++ src/api/Jobs/JobService.ts
@@ -1,8 +1,91 @@
 import ApiService, { ApiData } from '@/api/ApiService';
 
+export type JobData = {
+  positionName: string;
+  jobType: string;
+  industryType: number;
+  jobDescription: string;
+  jobRequirements: string;
+  jobFlexibility: string;
+  hasDialysisSupport: boolean;
+  hasFlexibleSchedule: boolean;
+  physicalDemands: string;
+  salaryType: string;
+  country: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  address: string;
+  skills: number[];
+  latLong: number[];
+  salaryRange: number[];
+  isOpen: boolean;
+  scheduledType: string;
+};
+
+export type CreateJobData = {
+  positionName: string;
+  jobType: string;
+  industryType: number;
+  jobDescription: string;
+  jobRequirements: string;
+  jobFlexibility: string;
+  hasDialysisSupport: boolean;
+  hasFlexibleSchedule: boolean;
+  physicalDemands: string;
+  salaryType: string;
+  country: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  address: string;
+  skills: number[];
+  latLong: number[];
+  salaryRange: number[];
+  isOpen: boolean;
+  scheduledType: string;
+};
+
 export default class JobService {
   private static getJobUrl() {
     return 'jobs';
+  }
+
+  public static async createJob(createJobData: CreateJobData): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getJobUrl()}`,
+          method: 'POST',
+          data: {
+            positionName: createJobData.positionName,
+            jobType: createJobData.jobType,
+            industryType: createJobData.industryType,
+            jobDescription: createJobData.jobDescription,
+            jobRequirements: createJobData.jobRequirements,
+            jobFlexibility: createJobData.jobFlexibility,
+            hasDialysisSupport: createJobData.hasDialysisSupport,
+            hasFlexibleSchedule: createJobData.hasFlexibleSchedule,
+            physicalDemands: createJobData.physicalDemands,
+            salaryType: createJobData.salaryType,
+            country: createJobData.country,
+            city: createJobData.city,
+            state: createJobData.state,
+            postalCode: createJobData.postalCode,
+            address: createJobData.address,
+            skills: createJobData.skills,
+            latLong: createJobData.latLong,
+            salaryRange: createJobData.salaryRange,
+            isOpen: createJobData.isOpen,
+            scheduledType: createJobData.scheduledType,
+          },
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
   public static async getAll(): Promise<ApiData> {
@@ -25,6 +108,24 @@ export default class JobService {
       const response = await ApiService.request(
         {
           url: `${this.getJobUrl()}/${id}`,
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getRecommendedJobs(employeesId: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getJobUrl()}/recommended/employees`,
+          data: {
+            employee_id: employeesId,
+          },
           method: 'GET',
         },
         true,
