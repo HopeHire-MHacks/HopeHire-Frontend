55ca65d6502774eb26bd038a964209921d4f3dde
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-02-19 17:02:51 +0800

    :bug: fix api call

--- src/components/Employee/JobListings/JobListings.tsx
+++ src/components/Employee/JobListings/JobListings.tsx
@@ -4,19 +4,21 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import CardListWithHeader from '../CardListWithHeader';
 import ListingItem from '../ListingItem';
+import { useApi } from '@/api/ApiHandler';
 
 export const positions: JobData[] = [];
 
 const Listings = () => {
   const history = useHistory();
   const [listings, setListings] = useState<JobData[]>([]);
+  const [getOpenJobs] = useApi(() => JobService.getOpenJobs(), true, true, true);
 
   const onClick = () => {
     history.push(routes.employee.base + routes.employee.applications);
   };
 
   const getJobs = async () => {
-    const jobs = (await JobService.getOpenJobs()).data;
+    const jobs = (await getOpenJobs()).data;
     setListings(jobs);
   };
 
