ff9d4983708be495d4be756463a6743b1668585d
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-02-19 10:55:32 +0800

    :tada: add Button Navigation

--- src/components/Employee/JobListings/JobListings.tsx
+++ src/components/Employee/JobListings/JobListings.tsx
@@ -1,4 +1,6 @@
+import { routes } from '@/constants/routes';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import CardListWithHeader from '../CardListWithHeader';
 import ListingItem from '../ListingItem';
 
@@ -114,11 +116,16 @@ export const positions = [
 ];
 
 const Listings = () => {
+  const history = useHistory();
+  const onClick = () => {
+    history.push(routes.employee.base + routes.employee.applications);
+  };
   return (
     <CardListWithHeader
       header='Job Postings'
       description='Discover your next career opportunity with our extensive job listings.'
       buttonText='View Submitted Applications'
+      buttonOnClick={onClick}
     >
       {positions.map(position => (
         <ListingItem key={position.id} position={position} />
